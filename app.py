067d4b51bfeada8910dc3ba3b2be7f06529bdeae
sammiee5311 <sammiee5311@gmail.com>
2022-12-03 17:18:17 +0900

    Init

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+resource = Resource(attributes={SERVICE_NAME: "opentelemetry-jaeger-demo"})
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(OTLPSpanExporter(endpoint="http://localhost:4317"))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer(__name__)
+
+app = Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+
+
+@app.route("/")
+def main():
+    with trace.get_tracer(__name__).start_as_current_span("foo"):
+        with trace.get_tracer(__name__).start_as_current_span("bar"):
+            return {"response": "Hello World"}
+
+
+if __name__ == "__main__":
+    app.run()
